54e316c56ab845b3d81128d1eb69f71a81c0bf7d
AadityaDev <adityaverma.dev@gmail.com>
2016-11-11 16:58:22 +0530

    Project setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import {
+    StackConfig,
+    Stack,
+    Card,
+    ThrowEvent,
+    DragEvent,
+    SwingStackComponent,
+    SwingCardComponent, SwingModule
+} from 'angular2-swing';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp),
+    SwingModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  providers: [],
+})
+export class AppModule {
+}
