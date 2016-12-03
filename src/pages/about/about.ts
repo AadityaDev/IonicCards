cb2de51f5186d16e6519b93d556264ce79cf74a0
AadityaDev <adityaverma.dev@gmail.com>
2016-12-03 13:10:44 +0530

    job service added

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,15 +1,8 @@
-import {Component, ViewChild, ViewChildren, QueryList, NgModule} from '@angular/core';
-import {NavController, List} from 'ionic-angular';
-import {Http, Headers} from '@angular/http';
-import 'rxjs/Rx';
-import {
-    StackConfig,
-    Stack,
-    Card,
-    ThrowEvent,
-    DragEvent,
-    SwingStackComponent,
-    SwingCardComponent} from 'angular2-swing';
+import {Component, ViewChild, ViewChildren, QueryList} from "@angular/core";
+import {NavController, List} from "ionic-angular";
+import {Http, Headers} from "@angular/http";
+import "rxjs/Rx";
+import {SwingStackComponent, SwingCardComponent} from "angular2-swing";
 import {JobService} from "../../providers/job-service";
 import {JobApplicationDetail} from "../../models/JobApplicationDetail";
 import {MyRefersResponse} from "../../models/MyRefersResponse";
@@ -23,39 +16,59 @@ export class AboutPage {
 
   @ViewChild('myswing1') swingStack: SwingStackComponent;
   @ViewChildren('mycards1') swingCards: QueryList<SwingCardComponent>;
-  @ViewChild(List) activeJobList:List;
+  @ViewChild(List) activeJobList: List;
 
-  headers:Headers;
-  data:MyRefersResponse;
-  jobApplicationDetails:Array<JobApplicationDetail>;
+  headers: Headers;
+  data: MyRefersResponse;
+  jobApplicationDetailsPre: Array<JobApplicationDetail>;
+  jobApplicationDetails: Array<JobApplicationDetail>;
+  activeJobsSize: number;
+  visibleItem: number;
+  showActiveText: string;
 
-  constructor(public navCtrl: NavController,public http: Http,public jobService:JobService) {
+  constructor(public navCtrl: NavController, public http: Http, public jobService: JobService) {
     this.load();
-
-    this.headers=new Headers();
-    this.headers.append('Content-Type','application/json');
-    this.headers.append('Authorization','c082d3c8893cea72c8d69792d1fdc7bc');
-    // this.headers.append('Content-Type','application/json');
-    // this.headers.append('JsonStub-User-Key','b631b589-105b-4ed2-b8ba-ae3f9c486652');
-    // this.headers.append('JsonStub-Project-Key','6cea1e1c-1fb0-4488-a665-9b444faacc51');
-
-    console.log("Headers: "+this.headers);
-    // this.http.get('http://staging.myrefers.com/myrefers-api/employers/job-list?isCandidate=false',{headers:this.headers})
-    //   .map(data => data.json().results);
+    this.headers = new Headers();
+    this.headers.append('Content-Type', 'application/json');
+    this.headers.append('Authorization', 'c082d3c8893cea72c8d69792d1fdc7bc');
+    this.activeJobsSize = 0;
+    this.visibleItem = 2;
+    this.showActiveText = "Show All active Jobs";
   }
 
   ngAfterViewInit() {
   }
 
-  load(){
+  load() {
     this.jobService.loadJobApplicants()
-      .then(data=>{
-        this.data=data;
-        console.log("Data: "+JSON.stringify(this.data));
-        this.jobApplicationDetails=this.data.result;
-        console.log("JobApplicationDetail: "+JSON.stringify(this.jobApplicationDetails));
+      .then(data => {
+        this.data = data;
+        this.jobApplicationDetails = this.data.result;
+        this.initializeActiveJobList();
       });
   }
 
+  initializeActiveJobList() {
+    if (this.jobApplicationDetails) {
+      this.jobApplicationDetailsPre = [];
+      for (var i = 0; i < this.visibleItem; i++) {
+        this.jobApplicationDetailsPre[i] = this.jobApplicationDetails[i];
+      }
+      console.log("Active Job List: " + this.jobApplicationDetailsPre.length);
+    }
+  }
 
+  showAllActiveJobList() {
+    if (this.jobApplicationDetails) {
+      if (this.jobApplicationDetailsPre.length == 2) {
+        console.log("show Job List: " + this.jobApplicationDetailsPre.length);
+        this.jobApplicationDetailsPre = this.jobApplicationDetails;
+        this.showActiveText = "Show Less";
+      } else {
+        console.log("ShowAll Job List: " + this.jobApplicationDetailsPre.length);
+        this.initializeActiveJobList();
+        this.showActiveText = "Show All active Jobs";
+      }
+    }
+  }
 }
